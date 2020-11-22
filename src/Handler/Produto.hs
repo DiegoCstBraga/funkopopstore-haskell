5edf7ff0a1a01b83364cb1abd05d6fc27b091a30
DiegoCstBraga <diego.cstbraga@gmail.com>
2020-11-22 18:56:14 -0300

    Tabelas do Banco de Dados definidas

--- src/Handler/Produto.hs
+++ src/Handler/Produto.hs
@@ -1,32 +1,44 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.Produto where
 
 import Import
 import Tool
+
 --import Database.Persist.Postgresql
 
 -- (<$>) = fmap :: Functor f => (a -> b) -> f a -> f b
 -- (<*>) :: Applicative f => f (a -> b) -> f a -> f b
 formProduto :: Maybe Produto -> Form Produto
-formProduto prod = renderDivs $ Produto  
-    <$> areq textField (FieldSettings "Nome: " 
-                                      Nothing
-                                      (Just "hs12")
-                                      Nothing
-                                      [("class","myClass")]
-                       ) (fmap produtoNome prod)
-    <*> areq doubleField "Preco: " (fmap produtoPreco prod)
+formProduto prod =
+  renderDivs $
+    Produto
+      <$> areq
+        textField
+        ( FieldSettings
+            "Nome: "
+            Nothing
+            (Just "hs12")
+            Nothing
+            [("class", "myClass")]
+        )
+        (fmap produtoNome prod)
+      <*> areq textField "Franquia: " (fmap produtoFranquia prod)
+      <*> areq textField "Descrição: " (fmap produtoDesc prod)
+      <*> areq intField "Número de série: " (fmap produtoSerialNumber prod)
+      <*> areq doubleField "Preço: " (fmap produtoPreco prod)
 
 auxProdutoR :: Route App -> Maybe Produto -> Handler Html
 auxProdutoR rt produto = do
-    (widget,_) <- generateFormPost (formProduto produto)
-    defaultLayout $ do
-        [whamlet|
+  (widget, _) <- generateFormPost (formProduto produto)
+  defaultLayout $ do
+    [whamlet|
+
             <h1>
                  CADASTRO DE PRODUTO
             
@@ -34,30 +46,41 @@ auxProdutoR rt produto = do
                 ^{widget}
                 <input type="submit" value="Cadastrar">
         |]
-    
+
 getProdutoR :: Handler Html
 getProdutoR = auxProdutoR ProdutoR Nothing
-    
+
 postProdutoR :: Handler Html
 postProdutoR = do
-    ((resp,_),_) <- runFormPost (formProduto Nothing)
-    case resp of 
-         FormSuccess produto -> do 
-             pid <- runDB $ insert produto
-             redirect (DescR pid)
-         _ -> redirect HomeR
-    
--- SELECT * from produto where id = pid 
+  ((resp, _), _) <- runFormPost (formProduto Nothing)
+  case resp of
+    FormSuccess produto -> do
+      pid <- runDB $ insert produto
+      redirect (DescR pid)
+    _ -> redirect HomeR
+
+-- SELECT * from produto where id = pid
 getDescR :: ProdutoId -> Handler Html
-getDescR pid = do 
-    produto <- runDB $ get404 pid
-    (widget,_) <- generateFormPost formQt
-    defaultLayout [whamlet|
+getDescR pid = do
+  produto <- runDB $ get404 pid
+  (widget, _) <- generateFormPost formQt
+  defaultLayout
+    [whamlet|
+
         <h1>
             Nome: #{produtoNome produto}
         
         <h2>
-            Preco: #{produtoPreco produto}
+            Franquia: #{produtoFranquia produto}
+
+        <h2>
+            Descrição: #{produtoDesc produto}
+        
+        <h2>
+            Número de série: #{produtoSerialNumber produto}
+
+        <h2>
+            Preço: #{produtoPreco produto}
         
         <form action=@{ComprarR pid} method=post>
             ^{widget}
@@ -65,10 +88,12 @@ getDescR pid = do
     |]
 
 getListProdR :: Handler Html
-getListProdR = do 
-    -- produtos :: [Entity Produto]
-    produtos <- runDB $ selectList [] [Desc ProdutoPreco]
-    defaultLayout [whamlet|
+getListProdR = do
+  -- produtos :: [Entity Produto]
+  produtos <- runDB $ selectList [] [Desc ProdutoPreco]
+  defaultLayout
+    [whamlet|
+
             <table>
                 <thead>
                     <tr>
@@ -100,25 +125,22 @@ getListProdR = do
     |]
 
 getUpdProdR :: ProdutoId -> Handler Html
-getUpdProdR pid = do 
-    antigo <- runDB $ get404 pid
-    auxProdutoR (UpdProdR pid) (Just antigo)    
-    
+getUpdProdR pid = do
+  antigo <- runDB $ get404 pid
+  auxProdutoR (UpdProdR pid) (Just antigo)
+
 -- UPDATE produto WHERE id = pid SET ...
 postUpdProdR :: ProdutoId -> Handler Html
 postUpdProdR pid = do
-    ((resp,_),_) <- runFormPost (formProduto Nothing)
-    case resp of 
-         FormSuccess novo -> do
-            runDB $ replace pid novo
-            redirect (DescR pid) 
-         _ -> redirect HomeR
+  ((resp, _), _) <- runFormPost (formProduto Nothing)
+  case resp of
+    FormSuccess novo -> do
+      runDB $ replace pid novo
+      redirect (DescR pid)
+    _ -> redirect HomeR
 
 postDelProdR :: ProdutoId -> Handler Html
-postDelProdR pid = do 
-    _ <- runDB $ get404 pid 
-    runDB $ delete pid 
-    redirect ListProdR
-
-
-
+postDelProdR pid = do
+  _ <- runDB $ get404 pid
+  runDB $ delete pid
+  redirect ListProdR
